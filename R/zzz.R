ba22ac3f5b463982de6c5c9136d6ac7b093bff70
muschellij2 <muschellij2@gmail.com>
2026-01-20 11:39:23 -0500

    adding in onload option for SWAN

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onLoad <- function(libname, pkgname) {
+  reticulate::py_require(
+    packages = c(
+      "SWaN_accel",
+      "scikit-learn==0.23.2",
+                 "pandas==1.1.3",
+                 "scipy==1.5.2",
+                 "numpy==1.19.2"),
+    python_version = "3.8")
+}
